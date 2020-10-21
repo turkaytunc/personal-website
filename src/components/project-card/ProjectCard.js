11b040606aa560586aa04960f717e2f5ed2521ca
Turkay Tunc <turkaytunc00@gmail.com>
2020-10-21 08:49:16 +0300

    add project-card

---
+++ src/components/project-card/ProjectCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const ProjectCard = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
+  );
+};
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { ProjectCard } from '../project-card/ProjectCard';
 import './projects.scss';
 
 const Projects = () => {
   document.title = 'Projects';
-  return <div className="projects-wrapper-div">Projects</div>;
+  return (
+    <div className="projects-wrapper-div">
+      <ProjectCard title={'Hello'}></ProjectCard>
+    </div>
+  );
 };
 
 export default Projects;
